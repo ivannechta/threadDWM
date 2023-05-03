fde819e9959912d2e4a42e8895d81a118f6b6376
ivannechta <tal200@lenta.ru>
2023-05-03 10:34:05 +0700

    Рефакторинг кода

--- ThreadDWM.cpp
+++ ThreadDWM.cpp
@@ -5,100 +5,42 @@
 int THREAD2_DELAY = 1;
 float Dispersion=1;
 
+volatile bool Flag=true; //Флаг окончания работ потоков
 
-using namespace std;
+Generator gen1(N), gen3(N), gen4(N);
+Consumer cons2(N);
+Statistic stat(N);
 
-// Распределения трех потоков. Необходимы для рассчета дивергенции Кульбака-Лейблера.
-float val1[N]; 
-float val2[N];
-float val3[N];
-float val4[N];
-
-float variable; //Наблюдаемая переменная. Один поток (генератор) заполняет её, а второй считывает значение.
-volatile bool Flag; //Флаг окончания работ потоков
-//static pthread_mutex_t cs_mutex =  PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP;
-
-class Gauss{
-    
-private:        
-    float function(float d,float m,float x){        
-        return 1.0f/(d*sqrt(2*3.14))*exp(-(x-m)*(x-m)/(2*d*d));
-    }
-
-public:
-    Gauss(){        
-        srand(time(NULL));
-    }
-    float GenNext(float _d,float _m){ //генерирует число в диапазоне [-5;5] по распределению Гаусса        
-        float r;
-        float x;        
-        int errorcnt=0;
-        do{
-            errorcnt++;
-            if (errorcnt>100) return 0; // Не удалось сгенерировать число - заменяем на ноль.
-            x=1.0f*(rand()%1000-500)/N;
-            r=0.001f*(rand()%1000);            
-        }while(r>function(_d,_m,x));        
-    return x;
-    }
-};
-Gauss G;
-void addVal(float *_val,float x){
-    //pthread_mutex_lock( &cs_mutex );
-    x=x*10.0f;
-    x=x+50;
-    if ((x<0)||(x>=N)) throw 1;
-    _val[int(x)]++;  
-    //pthread_mutex_unlock( &cs_mutex );  
-}
-float KL(float *_val1,float *_val2){ //Дивергенция Кульбака-Лейблера
-    float mesure=0;
-    float p,q;
-    float sum1=0,sum2=0;
-    for (int i=0;i<N;i++){
-        sum1+=_val1[i];
-        sum2+=_val2[i];
-    }
-    for (int i=0;i<N;i++){
-        p=1.0f*_val1[i]/sum1; 
-        q=1.0f*_val2[i]/sum2;
-        if ((q!=0)&&(p!=0)){
-            mesure+=(p*log2(p/q));            
-        }
-    }
-    return mesure;
-}
 void A(){   // Генератор распределения
     while(Flag==true){        
-        addVal(val1,variable=G.GenNext(1.0,0));                 
-        addVal(val3,G.GenNext(1.0,0));        
-        addVal(val4,G.GenNext(Dispersion,0));        
+        gen1.generate(1,0);
+        gen3.generate(1,0);
+        gen4.generate(Dispersion,0);
 
-        usleep(THREAD1_DELAY*1000);
+        usleep(THREAD1_DELAY*1000);// 1 мсек
     }    
 }
 void B(){   //Считыватель распределения
     float tmp;
-    while(Flag){        
-        addVal(val2,variable);         
-        usleep(THREAD2_DELAY*1000);
+    while(Flag){  
+        cons2.addVal(gen1.publicValue);
+
+        usleep(THREAD2_DELAY*1000); // 1 мсек
     }    
 }
 void C(){   //Вывод статистики
     for (int i=0;i<AMOUNT_DATA;i++){
         sleep(1);
-        //cout<<KL(val1,val2)<<"      "<<KL(val1,val3)<< "     "<<KL(val3,val2)<<"     "<<KL(val4,val2)<<"     "<<KL(val3,val4)<<endl;        
-        cout << KL(val3,val4)<<endl;
+        stat.showStatisticTab(gen1.values,cons2.values);
+        stat.showStatisticTab(gen1.values,gen3.values);
+        stat.showStatisticTab(gen3.values,cons2.values);
+        stat.showStatisticTab(gen4.values,cons2.values);
+        stat.showStatisticLn(gen3.values,gen4.values);        
     }
 }
-void Init(){    
-    Flag=true;
-    for (int i=0;i<N;i++){
-        val1[i]=val2[i]=val3[i]=val4[i]=0;
-    }
-}
-int main(int argc,char *argv[]){
-    /*if (argc!=3){
+
+int main(int argc,char *argv[]){ 
+    if (argc!=3){
         cout<<"Usage:\n";
         cout<<argv[0]<<" [Dispersion,float] [thread2 delay, (int msec)]\n";
 
@@ -108,19 +50,15 @@ int main(int argc,char *argv[]){
         return 0;
     }
     Dispersion=stof(argv[1]);
-    THREAD2_DELAY=atoi(argv[2]);        
-   */
-   Dispersion=4.5;
-   THREAD2_DELAY=1;        
-
-    Init();
+    THREAD2_DELAY=atoi(argv[2]);       
+   
+   
     std::thread thread1(A);  
     std::thread thread2(B);  
     std::thread thread3(C);  
-
-    //cout<<"Wait ..."<<endl;
+    
     thread3.join();    
-    //cout<<"Completed."<<endl;    
+    
     thread1.detach();
     thread2.detach();    
     return 0;
