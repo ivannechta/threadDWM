de4f3f8cc6b3cd11298294dac734060180e2cd06
ivannechta <tal200@lenta.ru>
2023-05-04 10:33:44 +0700

    Fix: Ошибка одинаковой дисперсии

--- ThreadDWM.cpp
+++ ThreadDWM.cpp
@@ -1,10 +1,8 @@
 #include "Header.h"
 #define N 100
-
-
 #define AMOUNT_DATA 50
-#define THREAD1_DELAY 1
-int THREAD2_DELAY = 1;
+
+int THREAD_DELAY = 1;
 float Dispersion=1;
 
 char Mode='s';
@@ -12,49 +10,49 @@ int TOTAL_GENERATORS=0;
 
 volatile bool Flag=true; //Флаг окончания работ потоков
 
-Generator gen1(N,1,0), gen3(N,1,0), gen4(N,Dispersion,0);
+Generator *gen1, *gen3, *gen4;
 Consumer cons2(N);
 Statistic stat(N);
 Generator **Generators;
 
-void SomeThread(){
+void SomeThread(){ //Множество генераторов (для оценки нагрузки на процессор)
     Generator tmp(N,1,0);
     while(Flag==true){               
         tmp.generate();
 
-        usleep(THREAD1_DELAY*1000);// 1 мсек
+        usleep(THREAD_DELAY*1000);// 1 мсек
     }    
 }
-void A(){   // Генератор распределения
+void A(){   // Генерация распределения
 
     while(Flag==true){               
-        gen1.generate();
-        gen3.generate();
-        gen4.generate();
+        gen1->generate();
+        gen3->generate();
+        gen4->generate();
 
-        usleep(THREAD1_DELAY*1000);// 1 мсек
+        usleep(THREAD_DELAY*1000);// 1 мсек
     }    
 }
 void B(){   //Считыватель распределения
     float tmp;
     while(Flag){  
-        cons2.addVal(gen1.publicValue);
+        cons2.addVal(gen1->publicValue);
 
-        usleep(THREAD2_DELAY*1000); // 1 мсек
+        usleep(THREAD_DELAY*1000); // 1 мсек
     }    
 }
 void C(){   //Вывод статистики
     for (int i=0;i<AMOUNT_DATA;i++){
         sleep(1);
         if (Mode=='s'){
-        stat.showStatisticTab(gen1.values,cons2.values);
-        stat.showStatisticTab(gen1.values,gen3.values);
-        stat.showStatisticTab(gen3.values,cons2.values);
-        stat.showStatisticTab(gen4.values,cons2.values);
-        stat.showStatisticLn(gen3.values,gen4.values);        
+        stat.showStatisticTab(gen1->values,cons2.values);
+        stat.showStatisticTab(gen1->values,gen3->values);
+        stat.showStatisticTab(gen3->values,cons2.values);
+        stat.showStatisticTab(gen4->values,cons2.values);
+        stat.showStatisticLn(gen3->values,gen4->values);        
         }
         if (Mode=='t'){
-            stat.showTime(&gen1);
+            stat.showTime(gen1);
         }
     }
 }
@@ -86,17 +84,21 @@ int main(int argc,char *argv[]){
             if (argc!=5) {Usage(argv[0]); return 0;}
             TOTAL_GENERATORS=stof(argv[2]);
             Dispersion=stof(argv[3]);
-            THREAD2_DELAY=atoi(argv[4]);            
+            THREAD_DELAY=atoi(argv[4]);            
             break;
         case 's':  
             if (argc!=4) {Usage(argv[0]); return 0;}
             Dispersion=stof(argv[2]);
-            THREAD2_DELAY=atoi(argv[3]);                
+            THREAD_DELAY=atoi(argv[3]);                
             break;
         default:
             Usage(argv[0]);
             return 0;
     }
+
+    gen1=new Generator(N,1,0);
+    gen3=new Generator(N,1,0);
+    gen4=new Generator(N,Dispersion,0);
 
     std::thread thread1(A);  
     std::thread thread2(B);  
@@ -107,7 +109,7 @@ int main(int argc,char *argv[]){
     for (int i=0;i<TOTAL_GENERATORS;i++){
         Threader[i]=new std::thread(SomeThread); 
     }
-    
+
     thread3.join();    
     
     thread1.detach();
